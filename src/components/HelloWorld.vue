dd0cb64ab2eeb42ddb673c597d41a3ec40359438
Luthfi Lisan Shidqi <luthfi.shidqi@biofarma.co.id>
2023-01-18 08:47:01 +0700

    update main

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Lutfi coba coba
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
